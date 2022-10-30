37914454e199312a962bc2ec43ad476c0cda9e1f
Charles Xu <char8x@gmail.com>
2022-10-30 11:24:28 +0800

    update at 2022-10-30 11:24:28 +0800

--- update-script.sh
+++ update-script.sh
@@ -5,4 +5,9 @@ curl https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/
 curl https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/China/China.list --output China.list
 curl https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/China/China_Domain.list --output China_Domain.list
 
+
+# 移除有问题的规则
+sed -i '' '/\.similarweb\.com/d' ./Advertising_Domain.list
+
+# 禁用规则
 rg --passthru 'URL-REGEX' -r '# URL-REGEX' Advertising.list > temp && mv temp Advertising.list
