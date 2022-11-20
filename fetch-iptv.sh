6d8a51643a20a1311a3ff1abd172c0cc840a9ffa
Charles Xu <char8x@gmail.com>
2022-11-20 21:12:47 +0800

    update at 2022-11-20 21:12:47 +0800

--- fetch-iptv.sh
+++ fetch-iptv.sh
@@ -3,5 +3,5 @@
 set -o errexit
 
 # 获取更新
-curl https://raw.githubusercontent.com/YueChan/IPTV/main/IPTV.m3u --output iptv.m3u
-curl https://raw.githubusercontent.com/YueChan/Radio/main/Radio.m3u --output radio.m3u
+curl https://raw.githubusercontent.com/YueChan/Live/main/IPTV.m3u --output iptv.m3u
+curl https://raw.githubusercontent.com/YueChan/Live/main/Radio.m3u --output radio.m3u
