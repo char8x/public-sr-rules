2465c86153ce1017b51f27e276db229356a08023
Charles Xu <char8x@gmail.com>
2022-11-20 22:38:55 +0800

    update at 2022-11-20 22:38:55 +0800

--- update-rule-script.sh
+++ update-rule-script.sh
@@ -5,7 +5,7 @@ set -o errexit
 # 获取更新
 curl https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/Advertising/Advertising.list --output Advertising.list
 curl https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/Advertising/Advertising_Domain.list --output Advertising_Domain.list
-curl https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/China/China.list --output China.list
+curl https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/ChinaMax/ChinaMax.list --output China.list
 curl https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Shadowrocket/China/China_Domain.list --output China_Domain.list
 
 # 移除多余的规则
