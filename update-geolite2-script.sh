6a7cd0cc8c0461452fd2dcf9a794f67d568fd4eb
Charles Xu <char8x@gmail.com>
2022-12-06 11:05:08 +0800

    update at 2022-12-06 11:05:08 +0800

--- update-geolite2-script.sh
+++ update-geolite2-script.sh
@@ -2,6 +2,6 @@
 
 set -o errexit
 
-# 获取更新
-curl -L https://git.io/GeoLite2-Country.mmdb --output GeoLite2-Country.mmdb
-curl -L https://git.io/GeoLite2-ASN.mmdb --output GeoLite2-ASN.mmdb
+# 获取更新 https://github.com/char8x/GeoLite.mmdb/tree/download
+#curl -L https://git.io/GeoLite2-Country.mmdb --output GeoLite2-Country.mmdb
+#curl -L https://git.io/GeoLite2-ASN.mmdb --output GeoLite2-ASN.mmdb
